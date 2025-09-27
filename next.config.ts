7c8b3557235d55c8dda67154722a8998bf201c5b
chukwuagoziesolomon <chukwuagoziesolomon.com>
2025-09-27 11:11:39 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
 };
 
 export default nextConfig;
